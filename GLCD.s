56c615c5c86752ff2da6fdc4f7624d4241194190
adr116 <adr116@skbl-422-006.ph.ic.ac.uk>
2020-11-13 11:53:36 +0000

    have added clear subroutine that erases entire GLCD screen 	have also added Asteroid image for later reference

--- GLCD.s
+++ GLCD.s
@@ -1,14 +1,19 @@
 #include <xc.inc>
 
-global	GLCD_setup, GLCD_write
+global	GLCD_setup, GLCD_Asteroid
 extrn	LCD_delay_ms
     
-psect	glcd_code,class=CODE
+psect	udata_acs   ;access ram for variables
+GLCD_ycounter:	ds  1	;for left/right
+GLCD_xcounter:	ds  1	;for up/down
 
 ;PORTB: 0=CS1 (1=left screen), 1=CS2 (1=right screen), 2=RS (0=instruction, 1=data)
     ;3=R/W (0=write, 1=read), 4=E (0=disable, 1=enable signal), 5=RST (0=reset)
     
         GLCD_E EQU 4		;defines pin 4 as enable
+	GLCD_RS EQU 2		;defines pin 2 as RS
+
+ psect	glcd_code,class=CODE
     
 GLCD_setup:
 	clrf	LATB, A
@@ -18,7 +23,7 @@ GLCD_setup:
 	clrf	TRISD, A	;turn off input for all TRISD
 	movlw	40
 	call	LCD_delay_ms
-	movlw	0x22		;turn on CS1, keep RST high (to avoid reset)
+	movlw	0x20		;turn on CS1&2, keep RST high (to avoid reset)
 	movwf	PORTB, A	
 	movlw	0x3F		;command to turn on GLCD
 	movwf	PORTD, A
@@ -35,16 +40,69 @@ GLCD_setup:
 	call	GLCD_enable
 	movlw	1
 	call	LCD_delay_ms
-	movlw	0xC0
+	movlw	0xC0		;command to set Z-address to 0
 	movwf	PORTD, A
 	call	GLCD_enable
 	movlw	1
 	call	LCD_delay_ms
+	call	GLCD_clear
 	return
-GLCD_write:
-	movlw	0x26		;turn on CS2, RS, & RST high to avoid reset
-	movwf	PORTB, A
-	movlw	0xF0		;sample data
+GLCD_clear:
+	movlw	0xB8
+	movwf	GLCD_xcounter, A    ;pages in each display
+clear_xloop:
+	call	GLCD_yclear
+	incf	GLCD_xcounter, F, A
+	movff	GLCD_xcounter, PORTD
+	call	GLCD_enable
+	movlw	0xBF
+	cpfsgt	GLCD_xcounter, A
+	bra	clear_xloop
+	return
+GLCD_yclear:
+	clrf	GLCD_ycounter, A    ;columns in each page
+clear_yloop:
+	call	blank
+	incf	GLCD_ycounter, F, A
+	movlw	0x3F		;63 as there are 64 columns
+	cpfsgt	GLCD_ycounter, A
+	bra	clear_yloop
+	return
+blank:
+	bsf	LATB, GLCD_RS, A
+	clrf	PORTD, A
+	call	GLCD_enable
+	bcf	LATB, GLCD_RS, A
+	return
+GLCD_Asteroid:
+	bsf	LATB, GLCD_RS, A    ;turns on RS pin to read/write data
+	call	Asteroid_P1	    ;draws rhombus drawing
+	call	Asteroid_P2
+	call	Asteroid_P3
+	call	Asteroid_P4
+	call	Asteroid_P4
+	call	Asteroid_P3
+	call	Asteroid_P2
+	call	Asteroid_P1
+	bcf	LATB, GLCD_RS, A    ;turns off RS pin to avoid read/write data
+	return
+Asteroid_P1:
+	movlw	0x18		;asteroid P1 is two dots on left/right
+	movwf	PORTD, A
+	call	GLCD_enable
+	return
+Asteroid_P2:
+	movlw	0x3C		;asteroid P2 is four dots
+	movwf	PORTD, A
+	call	GLCD_enable
+	return
+Asteroid_P3:	
+	movlw	0x7E		;asteroid P3 is six dots
+	movwf	PORTD, A
+	call	GLCD_enable
+	return
+Asteroid_P4:
+	movlw	0xFF		;asteroid P4 is eight dot middle
 	movwf	PORTD, A
 	call	GLCD_enable
 	return
@@ -65,5 +123,7 @@ GLCD_enable:
 	nop
 	nop
 	bcf	LATB, GLCD_E, A	;sets E off
+	movlw	0x01
+	call	LCD_delay_ms
 	return
 	
