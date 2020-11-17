6b567b149fd91acb975ad9ca974612a0133e5a39
adr116 <adr116@skbl-422-006.ph.ic.ac.uk>
2020-11-17 11:59:33 +0000

    have added subroutine for later experimentation, that is connected to PageLoop subroutine

--- Touch.s
+++ Touch.s
@@ -1,22 +1,21 @@
 #include <xc.inc>
 
-global	Boom
+global	Touch_Boom
     
 psect	udata_acs   ;access ram for variables
-Touch_boom:	ds  1
+Touch_counter:	ds  1
 
 ;PORTB: 0=CS1 (1=left screen), 1=CS2 (1=right screen), 2=RS (0=instruction, 1=data)
     ;3=R/W (0=write, 1=read), 4=E (0=disable, 1=enable signal), 5=RST (0=reset)
     	
-	AsteroidMove_RS EQU 2		;defines pin 2 as RS
-	AsteroidMove_RW	EQU 3		;defines pin 3 as R/W
-        AsteroidMove_E EQU 4		;defines pin 4 as enable
+	Touch_DRIVEA EQU 4		;defines pin 4 as DRIVEA
+        Touch_DRIVEB EQU 5		;defines pin 5 as DRIVEB
 
  psect	touch_code,class=CODE	
     
  
-Boom:
-    incf    Touch_boom
+Touch_Boom:
+    incf    Touch_counter, F, A
     return
 
 
