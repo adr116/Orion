f52d3fd0a1715643045b535405d62a9ad31f9d7a
adr116 <adr116@skbl-422-006.ph.ic.ac.uk>
2020-11-24 12:02:54 +0000

    have assigned ADCON0 correctly depending on pin being read

--- Touch.s
+++ Touch.s
@@ -28,38 +28,24 @@ Touch_Boom:
 	call    xaxis
 	movff	ADRESH, Touch_ADH, A	;stores high analogue bits for reference
 	movff	ADRESL, Touch_ADL, A	;stores low analogue bits for reference
-	call	comparison
+	;call	comparison
 	call	yaxis
 	movff	ADRESH, Touch_ADH, A
 	movff	ADRESL, Touch_ADL, A
-	call	comparison
+	;call	comparison
 	return
 xaxis:
-	bsf	TRISF, PORTF_RF2_POSN, A  ; pin RF2==AN7 input
-	banksel ANCON0
-	bsf	ANSEL7	    ; set AN7 to analog
-	movlw   0x01	    ; select AN7 for measurement
-	movwf   ADCON0, A   ; and turn ADC on
-	movlw   0x30	    ; Select 4.096V positive reference
-	movwf   ADCON1,	A   ; 0V for -ve reference and -ve input
-	movlw   0xF6	    ; Right justified output
-	movwf   ADCON2, A   ; Fosc/64 clock and acquisition times
+	movlw   00011101B        ; select AN7 for measurement and turn ADC on
+	movwf   ADCON0, A  
 	bcf	LATE, Touch_DRIVEA, A    ;deactivates yaxis measurement before xaxis
 	bsf	LATE, Touch_DRIVEB, A    ;activates xaxis measurement
-	movlw	0x01		    ;loop to slow 1
-	call	LCD_delay_ms
-	call	ADC_Read
+	movlw   0x01            ;loop to slow 1
+	call    LCD_delay_ms
+	call    ADC_Read
 	return
 yaxis:
-	bsf	TRISF, PORTF_RF5_POSN, A  ; pin RF5==AN10 input
-	banksel	ANCON0
-	bsf	ANSEL10	    ; set AN10 to analog
-	movlw   0x01	    ; select AN10 for measurement
-	movwf   ADCON0, A   ; and turn ADC on
-	movlw   0x30	    ; Select 4.096V positive reference
-	movwf   ADCON1,	A   ; 0V for -ve reference and -ve input
-	movlw   0xF6	    ; Right justified output
-	movwf   ADCON2, A   ; Fosc/64 clock and acquisition times
+	movlw	00101001B
+	movwf	ADCON0, A
 	bcf	LATE, Touch_DRIVEB, A    ;deactivates xaxis measurement before yaxis
 	bsf	LATE, Touch_DRIVEA, A    ;activates yaxis measurement
 	movlw	0x01
@@ -72,25 +58,17 @@ comparison:
 	call	BranchHub
 	return
 ADC_Setup:		    ;should initialize F2 & F5 pins for analogue
-	bcf	TRISE,	Touch_DRIVEA, A	    ;sets DRIVEA pin to input
-	bcf	TRISE,	Touch_DRIVEB, A	    ;sets DRIVEB pin to input
-	;bsf	TRISF, PORTF_RF2_POSN, A  ; pin RF2==AN7 input
-	;banksel ANCON0
-	;bsf	ANSEL7	    ; set AN7 to analog
-	;movlw   0x01	    ; select AN7 for measurement
-	;movwf   ADCON0, A   ; and turn ADC on
-	;movlw   0x30	    ; Select 4.096V positive reference
-	;movwf   ADCON1,	A   ; 0V for -ve reference and -ve input
-	;movlw   0xF6	    ; Right justified output
-	;movwf   ADCON2, A   ; Fosc/64 clock and acquisition times
-	;bsf	TRISF, PORTF_RF5_POSN, A  ; pin RF5==AN10 input
-	;bsf	ANSEL10	    ; set AN10 to analog
-	;movlw   0x01	    ; select AN10 for measurement
-	;movwf   ADCON0, A   ; and turn ADC on
-	;movlw   0x30	    ; Select 4.096V positive reference
-	;movwf   ADCON1,	A   ; 0V for -ve reference and -ve input
-	;movlw   0xF6	    ; Right justified output
-	;movwf   ADCON2, A   ; Fosc/64 clock and acquisition times
+	bcf	TRISE, Touch_DRIVEA, A ;sets DRIVEA pin to input
+	bcf	TRISE, Touch_DRIVEB, A ;sets DRIVEB pin to input
+	bsf	TRISF, PORTF_RF2_POSN, A ; pin RF2==AN7 input
+	bsf	TRISF, PORTF_RF5_POSN, A ; pin RF5==AN10 input
+	banksel ANCON0
+	bsf	ANSEL7		; set AN7 to analog
+	bsf	ANSEL10		; set AN10 to analog
+	movlw	0x30		; Select 4.096V positive reference
+	movwf	ADCON1, A	; 0V for -ve reference and -ve input
+	movlw	0xF6		; Right justified output
+	movwf	ADCON2, A	; Fosc/64 clock and acquisition times
 	return
 ADC_Read:
 	bsf	GO	    ; Start conversion by setting GO bit in ADCON0
